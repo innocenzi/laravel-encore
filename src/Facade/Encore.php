9065dfee79c95c31080257ba472d18482d4aaf3c
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-27 23:54:01 +0200

    fix: add php docblocks to facade

--- src/Facade/Encore.php
+++ src/Facade/Encore.php
@@ -4,6 +4,10 @@ namespace Innocenzi\LaravelEncore\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string getLinkTags(string $entryName)
+ * @method static string getScriptTags(string $entryName, bool $nodefer)
+ */
 class Encore extends Facade
 {
     protected static function getFacadeAccessor()
